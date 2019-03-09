ac987bd03b28dc90bcd2a855b252041955d38514
DESKTOP-R3QBLM3\Ishan Gupta <ishanguptajpr@gmail.com>
2019-03-09 18:41:44 +0530

    Renaming and moving CPP for organised structure.
    Renaming and moving CPP for organised structure.

--- Source/Testing_Grounds/Testing_GroundsGameMode.cpp
+++ Source/Testing_Grounds/Testing_GroundsGameMode.cpp
@@ -9,7 +9,7 @@ ATesting_GroundsGameMode::ATesting_GroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
